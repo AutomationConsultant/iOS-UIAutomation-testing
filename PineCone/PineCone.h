1cc4a6c66adc28d27aacc8201b5f598ffd4dccf2
Jesse Bounds <user@email.com>
2013-02-09 21:44:19 -0800

    added static library to hold tha goods

---
+++ PineCone/PineCone.h
@@ -0,0 +1,13 @@
+//
+//  PineCone.h
+//  PineCone
+//
+//  Created by Beth Bounds on 2/9/13.
+//  Copyright (c) 2013 Collins Middle School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PineCone : NSObject
+
+@end
---
+++ PineCone/PineCone.m
@@ -0,0 +1,13 @@
+//
+//  PineCone.m
+//  PineCone
+//
+//  Created by Beth Bounds on 2/9/13.
+//  Copyright (c) 2013 Collins Middle School. All rights reserved.
+//
+
+#import "PineCone.h"
+
+@implementation PineCone
+
+@end
