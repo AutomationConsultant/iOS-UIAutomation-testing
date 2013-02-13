f30ba74630d3485effabdb1559f5b7cc21f377c5
boundsj <jesse@rebounds.net>
2013-02-13 00:00:14 -0800

    updated to latest xcode, tested in 6.1 sim, fixed retain cycle issue in recursive block (just for fun)

--- AutomationTest/ViewController.m
+++ AutomationTest/ViewController.m
@@ -2,6 +2,14 @@
 #import "UIButton+PC.h"
 
 
+typedef void(^PushButtonBlock)();
+
+@interface ViewController ()
+
+@property (strong, nonatomic) PushButtonBlock pushButton;
+
+@end
+
 @implementation ViewController
 
 @synthesize button1;
@@ -14,21 +22,26 @@ static int blockIndex = 0;
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];    
+    [super viewDidLoad];
+    
     self.buttons = [NSMutableArray array];
     [self.buttons addObject:self.button1];
     [self.buttons addObject:self.button2];
     [self.buttons addObject:self.button3];
     [self.buttons addObject:self.button4];
+
+    __weak NSMutableArray *weakButtons = self.buttons;
+    __weak ViewController *weakSelf = self;
     
-    __block void(^pushButton)(void) = ^() {
-        UIButton *button = [self.buttons objectAtIndex:blockIndex++ % self.buttons.count];
+    self.pushButton = ^() {
+        UIButton *button = [weakButtons objectAtIndex:blockIndex++ % weakButtons.count];
         [button tap];        
         sleep(1);
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), pushButton);
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), weakSelf.pushButton);
     };
     
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), pushButton);
+    //dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), self.pushButton);
+    self.pushButton();
 }
 
 @end
